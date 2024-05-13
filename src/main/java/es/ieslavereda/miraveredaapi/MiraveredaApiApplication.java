ad961f585d67895a7f762e300bfd6a649c4a0364
JuanCarlosLaVereda <juasanper2@alu.edu.gva.es>
2024-05-13 20:27:37 +0200

    v.0.2

---
+++ src/main/java/es/ieslavereda/miraveredaapi/MiraveredaApiApplication.java
@@ -0,0 +1,13 @@
+package es.ieslavereda.miraveredaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiraveredaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiraveredaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/ieslavereda/miraveredaapi/MiraveredaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ieslavereda.miraveredaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiraveredaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
